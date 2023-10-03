7f17009c825aa64e19a1f977bde4e8494ab8f504
Matt Rakestraw <rake36@gmail.com>
2023-10-02 21:59:34 -0400

    first paycheck

--- allgood/Program.cs
+++ allgood/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using allgood;
+
 Console.WriteLine("All good!");
+
+var p = new Paycheck() { Amount = 5000 };
+Console.WriteLine(p.Amount);
