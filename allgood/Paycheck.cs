20695d543a17ed25543657e336e13c1dd28818bf
Matt Rakestraw <rake36@gmail.com>
2023-10-05 21:35:46 -0400

    fake paycheck

--- allgood/Paycheck.cs
+++ allgood/Paycheck.cs
@@ -8,6 +8,40 @@ namespace allgood
 {
     internal class Paycheck
     {
-        public decimal Amount { get; set; } 
+        public decimal GrossAmount { get; set; }
+        public decimal Oasdi { get; set; }
+        public decimal Medicare { get; set; }
+        public decimal FedTax { get; set; }
+        public decimal NcStateTax { get; set; }
+        public decimal Add { get; set; }
+        public decimal GroupTermLife { get; set; }
+        public decimal LongTermDisability { get; set; }
+        public decimal EeVoluntaryLife { get; set; }
+        public decimal HdHpPremium { get; set; }
+        public decimal HsaContribution { get; set; }
+        public decimal EsppContribution { get; set; }
+        public decimal Roth401kContribution { get; set; }
+
+        public decimal NetAmount => GrossAmount - Oasdi - Medicare - FedTax -
+            NcStateTax - Add - GroupTermLife - LongTermDisability - EeVoluntaryLife -
+            HdHpPremium - HsaContribution - EsppContribution - Roth401kContribution;
+
+        public static Paycheck Generate()
+        {
+            return new Paycheck()
+            {
+                GrossAmount = 6615.87m,
+                Oasdi = 412.28m,
+                Medicare = 96.42m,
+                FedTax = 1206.12m,
+                NcStateTax = 297m,
+                Add = 4.20m,
+                GroupTermLife = 7.87m,
+                LongTermDisability = 16.27m,
+                EeVoluntaryLife = 111.93m,
+                EsppContribution = 661.59m,
+                Roth401kContribution = 396.96m
+            };
+        }
     }
 }
--- allgood/Program.cs
+++ allgood/Program.cs
@@ -1,7 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using allgood;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 Console.WriteLine("All good!");
 
-var p = new Paycheck() { Amount = 5000 };
-Console.WriteLine(p.Amount);
+string environment = Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT") ?? throw new Exception("NETCORE_ENVIRONMENT variable not found");
+Console.WriteLine($"Environment: {environment}");
+
+var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{environment}.json", optional: true)
+            .AddUserSecrets<Program>()
+            .AddEnvironmentVariables();
+var configurationRoot = builder.Build();
+
+Paycheck? p0 = configurationRoot.GetSection("Paycheck").Get<Paycheck>();
+if (p0 == null) return;
+
+// Let's generate a bi-weekly paycheck calendar through end of 2026
+var firstDate = new DateTime(2023, 5, 18);
+var nextDate = firstDate;
+var cash = 0.0m;
+while (nextDate < new DateTime(2026, 12, 31))
+{
+    cash += p0.NetAmount;
+    Console.WriteLine($"{nextDate:yyyy-MM-dd dddd}:{cash:c}");
+    nextDate = nextDate.AddDays(14);
+}
+
