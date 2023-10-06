f9d5826a657b05fe55b0dccf2ab7b5b29ad6d8ad
Matt Rakestraw <rake36@gmail.com>
2023-10-05 21:40:46 -0400

    fake paycheck (#2)

--- allgood/Paycheck.cs
+++ allgood/Paycheck.cs
@@ -8,6 +8,22 @@ namespace allgood
 {
     internal class Paycheck
     {
-        public decimal Amount { get; set; } 
+        public decimal GrossAmount { get; set; }
+        public decimal Oasdi { get; set; }
+        public decimal Medicare { get; set; }
+        public decimal FedTax { get; set; }
+        public decimal NcStateTax { get; set; }
+        public decimal Add { get; set; }
+        public decimal GroupTermLife { get; set; }
+        public decimal LongTermDisability { get; set; }
+        public decimal EeVoluntaryLife { get; set; }
+        public decimal HdHpPremium { get; set; }
+        public decimal HsaContribution { get; set; }
+        public decimal EsppContribution { get; set; }
+        public decimal Roth401kContribution { get; set; }
+
+        public decimal NetAmount => GrossAmount - Oasdi - Medicare - FedTax -
+            NcStateTax - Add - GroupTermLife - LongTermDisability - EeVoluntaryLife -
+            HdHpPremium - HsaContribution - EsppContribution - Roth401kContribution;
     }
 }
